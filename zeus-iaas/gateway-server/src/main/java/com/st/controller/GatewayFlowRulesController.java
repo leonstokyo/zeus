7a9f13ae7796b69ed1dc5819e12924a173b0d6cb
leonseki <leonstokyo@gmail.com>
2024-01-08 01:08:40 +0900

    本地文件配置限流

---
+++ zeus-iaas/gateway-server/src/main/java/com/st/controller/GatewayFlowRulesController.java
@@ -0,0 +1,33 @@
+package com.st.controller;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+/**
+ * @author leon
+ * @date 2024/1/8 00:54
+ */
+@RestController
+public class GatewayFlowRulesController {
+    /**
+     * 获取当前系统的限流策略
+     */
+    @GetMapping("/gw/flow/rules")
+    public Set<GatewayFlowRule> getCurrentGatewayFlowRules() {
+        return GatewayRuleManager.getRules();
+    }
+
+    /**
+     * 获取定义的api分组
+     */
+    @GetMapping("/gw/api/groups")
+    public Set<ApiDefinition> getApiGroups() {
+        return GatewayApiDefinitionManager.getApiDefinitions();
+    }
+}
