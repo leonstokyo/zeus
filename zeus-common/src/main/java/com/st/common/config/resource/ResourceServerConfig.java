2952d3c239a3966c51745d82f01e343e2c71a09c
leon <leon.seki.tokyo@gmail.com>
2024-02-12 02:32:21 +0900

    common config

---
+++ zeus-common/src/main/java/com/st/common/config/resource/ResourceServerConfig.java
@@ -0,0 +1,68 @@
+package com.st.common.config.resource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.util.FileCopyUtils;
+
+/**
+ * @author leon
+ * @date 2024/2/12 02:03
+ */
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .sessionManagement().disable()
+                .authorizeRequests()
+                .antMatchers("/v2/api-docs",
+                        "/swagger-resources/configuration/ui",
+                        "/swagger-resources",
+                        "/swagger-resources/configuration/security",
+                        "webjars/",
+                        "/swagger-ui.html"
+                        ).permitAll()
+                .antMatchers("/**").authenticated()
+                .and().headers().cacheControl();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(jwtTokenStore());
+    }
+
+
+
+    private TokenStore jwtTokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        String s = null;
+        try {
+            ClassPathResource classPathResource = new ClassPathResource("coinexchange.pub");
+            byte[] bytes = FileCopyUtils.copyToByteArray(classPathResource.getInputStream());
+            s = new String(bytes, "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        tokenConverter.setVerifierKey(s);
+        return tokenConverter;
+    }
+
+
+}
